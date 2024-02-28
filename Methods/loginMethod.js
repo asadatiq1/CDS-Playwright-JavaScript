8f8b08432c200cc8648d81ae248182a1d2228692
Asad Atiq <124272323+asadatiq1@users.noreply.github.com>
2024-02-28 16:59:50 +0500

    Completed Login

--- Methods/loginMethod.js
+++
@@ -1,8 +0,0 @@
-import { utils } from "../CommonUtility/utils";
-
-exports.loginMethod = class loginMethod {
-  constructor(page) {
-    this.page = page;
-    this.utils = new utils();
-  }
-};
--- Test_Files/TC_Login.spec.js
+++ Test_Files/TC_Login.spec.js
@@ -24,3 +24,75 @@ test("Verify user can login with registered email", async ({ page }) => {
   await comon.click(loginData.lginBtn);
   await comon.text(dashData.header, dashData.headerText);
 });
+
+test("Verify login with unregistered email", async ({ page }) => {
+  await comon.enter(loginData.emailField, data.UnregisteredEmail);
+  await comon.enter(loginData.passwordField, data.WrongPassword);
+  await comon.click(loginData.lginBtn);
+  await comon.text(
+    loginData.UnregisteredError,
+    loginData.UnregisteredErrorText
+  );
+});
+
+test("Veify click login button without entering email and a password", async ({
+  page,
+}) => {
+  await comon.click(loginData.lginBtn);
+  await comon.text(
+    loginData.RequiredEmailError,
+    loginData.RequiredEmailErrorText
+  );
+  await comon.text(
+    loginData.RequiredPasswordError,
+    loginData.RequiredPasswordErrorText
+  );
+});
+
+test("Verify click login without entering email", async ({ page }) => {
+  await comon.enter(loginData.passwordField, data.RegisteredPassword);
+  await comon.click(loginData.lginBtn);
+  await comon.text(
+    loginData.RequiredEmailError,
+    loginData.RequiredEmailErrorText
+  );
+});
+
+test("Verify click login without entering password", async ({ page }) => {
+  await comon.enter(loginData.emailField, data.RegisteredEmail);
+  await comon.click(loginData.lginBtn);
+  await comon.text(
+    loginData.RequiredPasswordError,
+    loginData.RequiredPasswordErrorText
+  );
+});
+
+test("Verify enter invalid email", async ({ page }) => {
+  await comon.enter(loginData.emailField, data.InvalidEmail);
+  await comon.enter(loginData.passwordField, data.RegisteredPassword);
+  await comon.click(loginData.lginBtn);
+  await comon.text(
+    loginData.InvalidEmailError,
+    loginData.InvalidEmailErrorText
+  );
+});
+
+test("Verify enter invalid password", async ({ page }) => {
+  await comon.enter(loginData.emailField, data.RegisteredEmail);
+  await comon.enter(loginData.passwordField, data.InvalidEmail);
+  await comon.click(loginData.lginBtn);
+  await comon.text(
+    loginData.InvalidEmailError,
+    loginData.InvalidEmailErrorText
+  );
+});
+
+test("Enter wrong password for register email", async ({ page }) => {
+  await comon.enter(loginData.emailField, data.RegisteredEmail);
+  await comon.enter(loginData.passwordField, data.WrongPassword);
+  await comon.click(loginData.lginBtn);
+  await comon.text(
+    loginData.WrongEmailPasswordError,
+    loginData.WrongEmailPasswordErrorText
+  );
+});
